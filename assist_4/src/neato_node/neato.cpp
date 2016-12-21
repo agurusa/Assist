4edbce301a9e935dca541a500c25b9f387fd9712
Aarthi <agurusa@gmail.com>
2016-12-20 20:11:50 -0800

    Add comments to neato.cpp file

--- assist_4/src/neato_node/neato.cpp
+++ assist_4/src/neato_node/neato.cpp
@@ -9,18 +9,30 @@ using namespace std;
 
 int main(int argc, char **argv){
 
+	// set serialDev variable to the same port that the neato will be plugged in to the raspberry pi
 	string serialDev ("/dev/ttyACM0");
+
 	string getVersion = "getVersion\n";
 	string turnOnTestmode = "Testmode on\n";
 	string command = "Setmotor -300 300 300\n";
+
+	// create a new serial port connection using the SimpleSerial class.
 	SimpleSerial serial(serialDev, 115200);
+
+	// send a simple command to make sure the connection has been made.
 	serial.writeString(getVersion);
+	// read the response from that command
 	serial.readLine();
-	sleep(2);
+	// wait for 1 second.
+	sleep(1);
+
+	// put the neato on testmode.
 	serial.writeString(turnOnTestmode);
-	sleep(2);
+	sleep(1);
+	// make the neato move.
 	serial.writeString(command);
-	sleep(2);
+	sleep(1);
+	
 	return 0;
 
 };
